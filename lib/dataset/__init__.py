ecf24db31a6443a1323a065736ed7a9a48195c10
Alex Zhu <alexzhu@seas.upenn.edu>
2019-10-12 17:02:35 -0400

    Handled pretrained models properly, added combined h36m, mpii dataset

--- lib/dataset/__init__.py
+++ lib/dataset/__init__.py
@@ -11,4 +11,5 @@ from __future__ import print_function
 from .mpii import MPIIDataset as mpii
 from .event_mpii import EventMPIIDataset as event_mpii
 from .DHP19Dataset import DHP19Dataset as dhp19
+from .event_comb_mpii_h36m import get_joint_dataset as event_comb_mpii_h36m
 #from .coco import COCODataset as coco
